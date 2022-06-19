0a8c9ffd28c0ceb965dd945c90bb8d79475694a2
jdmsharpe <jdmsharpe@gmail.com>
2022-06-18 22:07:53 -0700

    Add bounds on search depth/difficulty

--- inc/AI.h
+++ inc/AI.h
@@ -18,7 +18,18 @@ public:
   inline std::optional<Color> getColor() { return m_color; }
   inline void setColor(Color color) { m_color = color; }
 
-  inline void setDifficulty(int increment) { m_difficulty += increment; }
+  inline void setDifficulty(int increment) {
+    // Min is indisputably 1, 0 causes a softlock
+    // Max could be higher, but a depth of 5 is very slow (1 min or more)
+    // so no real reason to go beyond it
+    if (m_difficulty + increment < 1) {
+      return;
+    } else if (m_difficulty + increment > 5) {
+      return;
+    }
+
+    m_difficulty += increment;
+  }
 
   std::pair<Position, Position> minimaxRoot(Color max);
   int minimax(Color max, int depth, int alpha, int beta);
